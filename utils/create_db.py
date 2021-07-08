5db45b1bcc35e66771cc856132fb337d02373611
MargoRS <slava1kvartovkin@gmail.com>
2021-07-08 20:12:30 +0300

    fix: Procfile

--- utils/create_db.py
+++ utils/create_db.py
@@ -5,7 +5,7 @@ from utils.db import conn, insert_user
 def create_db():
     with conn.cursor() as cursor:
         create_roles = "CREATE TYPE roles AS ENUM ('superadmin', 'admin', 'user');"
-        create_users = "CREATE TABLE users(id bigint PRIMARY KEY, role roles);"
+        create_users = "CREATE TABLE users(id bigint PRIMARY KEY, role roles, url VARCHAR(100));"
         create_blacklist = "CREATE TABLE blacklist(id bigint PRIMARY KEY, url VARCHAR(100), added_by roles);"
 
         create_array = [create_roles, create_users, create_blacklist]
@@ -14,7 +14,6 @@ def create_db():
             cursor.execute(create)
         conn.commit()
 
-        # insert_user(conn, 627775883, 'superadmin', url='@iamsvyatoslav')
-
 
 create_db()
+# insert_user(conn, 627775883, 'superadmin', url='@iamsvyatoslav')
