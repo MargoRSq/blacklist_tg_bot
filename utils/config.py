b52cc87625ab1e0ab97e2c2b8b8a5076d1dd5118
Margo <slava1kvartovkin@gmail.com>
2021-09-18 18:46:05 +0300

    fix: url for heroku

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,9 @@
 from starlette.config import Config
-
+from re import sub
 
 config = Config(".env")
 
 TOKEN: str = config("TOKEN")
 
-DATABASE_URL: str = config("DATABASE_URL")
-
+DATABASE_URL: str = config("SQLALCH_DATABASE_URL")
 TRASH_CHAT_ID: int = int(config("TRASH_CHAT_ID"))
